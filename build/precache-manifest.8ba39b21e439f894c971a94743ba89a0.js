79873cc95501a6b7ee4dccbb7c7abae6017675a2
Archana <archanass101@gmail.com>
2019-11-23 16:57:10 +0530

    done

---
+++ build/precache-manifest.8ba39b21e439f894c971a94743ba89a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a237e1bc85c9664021761d2addcea886",
+    "url": "/index.html"
+  },
+  {
+    "revision": "061bdae7bcd48d411bc2",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "009f80395b91ac2a34fb",
+    "url": "/static/js/2.84b26f6d.chunk.js"
+  },
+  {
+    "revision": "061bdae7bcd48d411bc2",
+    "url": "/static/js/main.31fe1d93.chunk.js"
+  },
+  {
+    "revision": "76ebfb067512e4175dad",
+    "url": "/static/js/runtime~main.30553761.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
